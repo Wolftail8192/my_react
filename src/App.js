512bab89e36052f4c13148598bc3006088864a87
Wolftail8192 <4420299@gmail.com>
2022-01-16 15:36:44 +0200

    base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Users from './Components/Users/Users';
+import UserDetails from './Components/UserDetails/UserDetails';
+import css from './App.module.css';
+
 
 const App = () => {
     return (
         <div>
-          
+          <div className={css.wrap}>
+              <Users/>
+              <UserDetails/>
+          </div>
+            <Posts/>
         </div>
     );
 };
