5c582fe33d8ef27cfe3f514eba7a261618b5cc77
Wolftail8192 <4420299@gmail.com>
2022-01-16 15:46:01 +0200

    base1.1

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,1 +1,7 @@
-const
+import {axiosService} from "./axios.service";
+import {urls} from "./configs/urls";
+
+
+export const postService = {
+    getByUserId:(id)=>axiosService.get(`${urls.posts}?userId=${id}`)
+}
