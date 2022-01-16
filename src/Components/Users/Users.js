ceb1452139f47c25b6f48c4db670c8068fb18262
Wolftail8192 <4420299@gmail.com>
2022-01-16 17:13:48 +0200

    base1.1

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=>{
+        userService.getAll().then(value => setUsers(value))
+
+    }, [])
+
+
     return (
         <div>
-            
+            {users.map(user => <User key={user.id} user={user}/>)}
         </div>
     );
 };
